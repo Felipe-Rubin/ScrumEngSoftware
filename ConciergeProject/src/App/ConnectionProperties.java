a7c8e5d65d4cbea6d72b82914b8cdc030299cf42
Felipe Rubin <felipeprubin@gmail.com>
2017-05-09 12:54:27 -0300

    Parte de conexão com o BD

---
+++ ConciergeProject/src/App/ConnectionProperties.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package App;
+
+/**
+ *
+ * @author Felipe Rubin
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.*;
+
+public class ConnectionProperties {
+    
+    private String hostname, port, database, user, password;
+    
+    public void setHost(String hostname){this.hostname = hostname;}
+    public void setPort(String port){this.port = port;}
+    public void setDB(String database){this.database = database;}
+    public void setUser(String user){this.user = user;}
+    public void setPass(String password){this.password = password;}
+    
+    public String getHost(){return hostname;}
+    public String getPort(){return port;}
+    public String getDB(){return database;}
+    public String getUser(){return user;}
+    public String getPass(){return password;}
+    
+    public String getUrl(){
+        return "jdbc:mysql://"+hostname+":"+port+"/"+database;
+    }
+    
+    public boolean isOK(){
+    
+        if(getHost() != null && getPort() != null && getDB()!= null && getUser()!= null && getPass() != null){
+        
+            return true;
+        }
+        return false;
+    }
+}
---
+++ ConciergeProject/src/App/ServerConnect.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package App;
+
+/**
+ *
+ * @author Felipe Rubin
+ */
+
+
+//import ConnectionProperties;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ServerConnect {
+    private Connection connection = null;
+    private ConnectionProperties cprop;
+    
+    public ConnectionProperties getConnectionProp() {
+        return cprop;
+    }
+    
+    public void setConnectionProp(ConnectionProperties cprop){
+        this.cprop = cprop;
+    }
+    
+    public void setConnection(Connection connection){
+    
+        this.connection = connection;
+    }
+    
+    public Connection getConnection() {
+        connection = getConnectionMySQL(cprop);
+        return connection;
+    }
+    
+    public ServerConnect(ConnectionProperties cprop) {
+        this.cprop = cprop;
+
+    }
+    
+    public static Connection getConnectionMySQL(ConnectionProperties cprop) {
+
+        Connection connection = null;
+        
+        try{
+        
+            String driverName = "com.mysql.jdbc.Driver";
+            Class.forName(driverName);
+            connection = DriverManager.getConnection(cprop.getUrl(), cprop.getUser(), cprop.getPass());
+            
+            if(connection != null){          
+              return connection;              
+            }else{return null;}
+        }catch (ClassNotFoundException | SQLException e) {//Driver nao encontrado
+            return null;
+        }
+    }
+    
+    public static boolean CloseConnection(Connection c) {
+
+        try {
+            c.close();
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+    
+    
+    public ResultSet QueryEndereco(){
+        Statement st = null;
+        String sql;
+        
+        try{
+            st = connection.createStatement();
+            sql = "SELECT * FROM Endereco";
+            ResultSet rs = st.executeQuery(sql);
+            return rs;            
+        }catch (SQLException ex){
+            Logger.getLogger(ServerConnect.class.getName()).log(Level.SEVERE, null, ex);
+            return null;         
+        }        
+    }
+    
+    
+    public ResultSet QueryPedido(){
+        Statement st = null;
+        String sql;
+        
+        try{
+            st = connection.createStatement();
+            sql = "SELECT * FROM Pedido";
+            ResultSet rs = st.executeQuery(sql);
+            return rs;            
+        }catch (SQLException ex){
+            Logger.getLogger(ServerConnect.class.getName()).log(Level.SEVERE, null, ex);
+            return null;         
+        }        
+    }
+        public ResultSet QueryProfile(){
+        Statement st = null;
+        String sql;
+        
+        try{
+            st = connection.createStatement();
+            sql = "SELECT * FROM Profile";
+            ResultSet rs = st.executeQuery(sql);
+            return rs;            
+        }catch (SQLException ex){
+            Logger.getLogger(ServerConnect.class.getName()).log(Level.SEVERE, null, ex);
+            return null;         
+        }        
+    }
+    
+
+    
+    public ResultSet QueryGeneric(String sql){ //Generica requer input da Sql
+    
+        Statement st = null;
+        
+        try{
+            st = connection.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            return rs;           
+        }catch (SQLException ex){
+            Logger.getLogger(ServerConnect.class.getName()).log(Level.SEVERE, null, ex);
+            return null;          
+        }
+    }
+}
